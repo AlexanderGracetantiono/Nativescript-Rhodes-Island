6d5b343d9ed53e59b4ddfd0f8e1680697095ff23
AlexanderGracetantiono <alexander.remains.1247@gmail.com>
2020-05-19 20:10:48 +0700

    Added login google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { UserService } from "../services/user.service";
 import { Router } from "@angular/router";
 import { Page } from "tns-core-modules/ui/page";
 import * as firebase from "nativescript-plugin-firebase";
+import { ActivityIndicator } from "tns-core-modules/ui/activity-indicator";
+import { EventData } from "tns-core-modules/data/observable";
 // const firebase = require("nativescript-plugin-firebase");
 @Component({
   selector: 'ns-login',
@@ -14,14 +16,17 @@ import * as firebase from "nativescript-plugin-firebase";
 export class LoginComponent implements OnInit {
   user_entity: User;
   isLogin = true;
+  isLoading = false;
   constructor(
     private userservice: UserService,
     private router: Router,
     private page: Page,
   ) {
     this.user_entity = new User();
-    this.user_entity.email = "alexander.remains.1247@gmail.com"
-    this.user_entity.password = "8gatsunonikki"
+    this.user_entity.email = "a@gmail.com"
+    this.user_entity.password = "hello1"
+    // this.user_entity.email = ""
+    // this.user_entity.password = ""
   }
 
   ngOnInit(): void {
@@ -54,42 +59,72 @@ export class LoginComponent implements OnInit {
     //   }
     // );
   }
+  onBusyChanged(args: EventData) {
+    let indicator: ActivityIndicator = <ActivityIndicator>args.object;
+    console.log("indicator.busy changed to: " + indicator.busy);
+  }
   handleSubmit() {
+    this.isLoading = true;
     if (this.isLogin) {
       this.handleLogin();
     } else {
       this.handleSignUp();
     }
   }
-
+  handleLoginGoogle() {
+    this.isLoading = true;
+    this.userservice.loginGoogle().then((res) => {
+      this.isLoading = false;
+    }, (err) => {
+      this.isLoading = false;
+      alert({
+        title: "Login Invalid",
+        message: "Email tidak terdaftar",
+        okButtonText: "OK, got it"
+      });
+      alert(err);
+    })
+  }
   handleLogin() {
     // alert("You're using: " + this.user_entity.email);
-    firebase.login(
-      {
-        type: firebase.LoginType.PASSWORD,
-        passwordOptions: {
-          email: this.user_entity.email,
-          password: this.user_entity.password
-        }
-      })
-      .then(result => {
-        console.log("Hasil UID:", result.uid);
-        if (result.uid != null) {
-          this.router.navigate(["/home"])
-        }
-        // var data = JSON.stringify(result)
-        // data.search('uid');
-      })
-      .catch(error => {
-        alert({
-          title: "Login Invalid",
-          message: "Email dan Password salah",
-          okButtonText: "OK, got it"
-        })
-        console.log("error:", JSON.parse(error))
-      }
+    this.userservice.login(this.user_entity).then((result) => {
+      this.isLoading = false;
+      this.router.navigate(["/home"])
+    }, (err) => {
+      this.isLoading = false;
+      alert({
+        title: "Login Invalid",
+        message: "Email dan Password salah",
+        okButtonText: "OK, got it"
+      });
+      console.log(err);
+    })
+    // firebase.login(
+    //   {
+    //     type: firebase.LoginType.PASSWORD,
+    //     passwordOptions: {
+    //       email: this.user_entity.email,
+    //       password: this.user_entity.password
+    //     }
+    //   })
+    //   .then(result => {
+    //     console.log("Hasil UID:", result.uid);
+    //     if (result.uid != null) {
+    //       this.router.navigate(["/home"])
+    //     }
+    //     // var data = JSON.stringify(result)
+    //     // data.search('uid');
+    //   })
+    //   .catch(error => {
+    //     alert({
+    //       title: "Login Invalid",
+    //       message: "Email dan Password salah",
+    //       okButtonText: "OK, got it"
+    //     })
+    //     console.log("error:", JSON.parse(error))
+    //   }
 
-      );
+    //   );
     // firebase.auth().signInWithEmailAndPassword(this.user_entity.email,this.user_entity.password)
     // .then(() => console.log("User logged in"))
     // .catch(err => console.log("Login error: " + JSON.stringify(err)));
@@ -106,28 +141,22 @@ export class LoginComponent implements OnInit {
     // );
   }
   handleSignUp() {
-    firebase.createUser({
-      email: this.user_entity.email,
-      password: this.user_entity.password
+    this.userservice.register(this.user_entity).then((res) => {
+      this.isLoading = false;
+      alert({
+        title: "Congrats",
+        message: "User created",
+        okButtonText: "Nice!"
+      })
+    }, (err) => {
+      this.isLoading = false;
+      alert({
+        title: "Cannot create user",
+        message: err,
+        okButtonText: "OK, got it"
+      })
     })
-      .then(
-        function (result) {
-          console.log("Created", result);
-          this.handleChangeState();
-          alert({
-            title: "Congrats",
-            message: "User created",
-            okButtonText: "Nice!"
-          })
-        },
-        function (errorMessage) {
-          console.log("Error");
-          alert({
-            title: "No user created",
-            message: errorMessage,
-            okButtonText: "OK, got it"
-          })
-        })
+
     // this.userservice.register(this.user_entity)
     //   .subscribe(
     //     () => {
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,43 +11,60 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     register(user: User) {
-        // if (!user.email || !user.password) {
-        //     return throwError("Please provide both an email address and password.");
-        // }
-
-        // return this.http.post(
-        //     Config.apiUrl + "user/" + Config.appKey,
-        //     JSON.stringify({
-        //         username: user.email,
-        //         password: user.password
-        //     }),
-        //     { headers: this.getCommonHeaders() }
-        // ).pipe(
-        //     catchError(this.handleErrors)
-        // );
-        return firebase.createUser({
-            email:user.email,
-            password:user.password
+        console.log("user",user.email,user.password)
+        return new Promise((resolve, reject) => {
+            firebase.createUser({
+                email: user.email,
+                password: user.password
+              }).then(
+                function (result) {
+                    JSON.stringify(result);
+                    resolve(result);
+                },
+                function (errorMessage) {
+                    console.log(errorMessage);
+                    reject(errorMessage);
+                }
+            );
         })
-        .then(
-            (res)=>console.log("reg res:",res)
-        )
+    }
+    loginGoogle() {
+        return new Promise((resolve, reject) => {
+            firebase.login({
+                type: firebase.LoginType.GOOGLE,
+            }).then(
+                function (result) {
+                    JSON.stringify(result);
+                    resolve(result);
+                },
+                function (errorMessage) {
+                    console.log(errorMessage);
+                    reject(errorMessage);
+                }
+            );
+        })
     }
     login(user: User) {
-        return this.http.post(
-            Config.apiUrl + "user/" + Config.appKey + "/login",
-            JSON.stringify({
-                username: user.email,
-                password: user.password
-            }),
-            { headers: this.getCommonHeaders() }
-        ).pipe(
-            map(response => response),
-            tap(data => {
-                Config.token = (<any>data)._kmd.authtoken
-            }),
-            catchError(this.handleErrors)
-        );
+        // console.log("user",user.email,user.password)
+        return new Promise((resolve, reject) => {
+            firebase.login({
+                type: firebase.LoginType.PASSWORD,
+                passwordOptions: {
+                    email: user.email,
+                    password: user.password
+                }
+            }).then(
+                function (result) {
+                    JSON.stringify(result);
+                    // console.log(result.email)
+                    resolve(result.email);
+                },
+                function (errorMessage) {
+                    console.log(errorMessage);
+                    reject(errorMessage);
+                }
+            );
+        })
     }
     getCommonHeaders() {
         return {
